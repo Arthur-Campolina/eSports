1d67b842b9a5dc8923d9aed8c63428212addd8f7
Arthur Campolina <arthurcampolina@hotmail.com>
2022-09-15 11:57:05 -0300

    web - styling with tailwindcss + phosphor

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,83 @@
-interface ButtonProps {
-  title: string;
-}
+import "./styles/main.css";
 
-function Button(props: ButtonProps) {
-  return <button>{props.title}</button>;
-}
+import logoImg from "./assets/logo.svg";
 
 function App() {
   return (
-    <div>
-      <Button title="send 1" />
-      <Button title="send 2" />
-      <Button title="send 2" />
+    <div className="max-w-[1344px] mx-auto my-20 flex flex-col items-center">
+      <img src={logoImg} />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 1.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative" rounded-lg overflow-hidden>
+          <img src="/image 2.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative" rounded-lg overflow-hidden>
+          <img src="/image 3.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">CS Go</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative" rounded-lg overflow-hidden>
+          <img src="/image 5.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative" rounded-lg overflow-hidden>
+          <img src="/image 6.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortnite</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative" rounded-lg overflow-hidden>
+          <img src="/image 7.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              World of Warcraft
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players!
+            </span>
+            <strong className="text=2xl text-white font-black block">
+              Não encontrou seu duo
+            </strong>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded">
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
